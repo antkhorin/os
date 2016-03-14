fdf6808b8b0c8674e2a272d91f824a81a8644a06
khorinanton <anton08082010@mail.ru>
2016-03-14 20:27:09 +0300

    fixfix

--- cat/cat.c
+++ cat/cat.c
@@ -6,7 +6,7 @@ int main()
 {
 	ssize_t count;
 	void *buf;
-	const size_t BUF_SIZE;
+	const size_t BUF_SIZE = 256;
 	buf = malloc(BUF_SIZE * sizeof(char));
 	while (count = read(STDIN_FILENO, buf, BUF_SIZE))
 	{
