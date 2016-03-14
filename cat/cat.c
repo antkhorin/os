35e0e0b8d8c7e0881fdbe1ce0c4ea5043aca96d9
khorinanton <anton08082010@mail.ru>
2016-03-14 20:22:17 +0300

    fixfix

---
+++ cat/cat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+	ssize_t count;
+	void *buf;
+	const size_t BUF_SIZE;
+	buf = malloc(BUF_SIZE * sizeof(char));
+	while (count = read(STDIN_FILENO, buf, BUF_SIZE))
+	{
+		write(STDOUT_FILENO, buf, count);
+	}
+	close(STDOUT_FILENO);
+	free(buf);
+	return 0;
+}
