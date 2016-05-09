188e5efdac049de43a17dc23994bb26efa49ce03
khorinanton <antkhorin@gmail.com>
2016-05-10 01:42:02 +0300

    badlinks

---
+++ badlinks/badlinks.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+recurse() {
+    for file in $1/*; do
+        if [ -d "$file" ] 
+            then recurse $file
+	elif [ -h "$file" -a ! -e "$file" -a `stat --format=%Y $file` -le $(( `date +%s` - 604800 )) ] 
+            then echo "$file"
+	fi
+	done
+}
+recurse $1
